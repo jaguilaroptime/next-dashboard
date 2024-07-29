a7ab6c20356a213418697790cbb0014dbe2e6b89
Jeenson Aguilar <jaguilar@optimeconsulting.com>
2024-07-29 16:56:43 -0500

    Update and delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import pool from "@/app/lib/mysql";
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -31,5 +32,34 @@ export async function createInvoice(formData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-
   }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  const db = await pool.getConnection();
+  await db.execute(`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = '${status}'
+    WHERE id = ${id}
+  `);
+  db.release();
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  const db = await pool.getConnection();
+  await db.execute(`DELETE FROM invoices WHERE id = ${id}`);
+  db.release();
+  revalidatePath('/dashboard/invoices');
+}
